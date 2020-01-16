41bab554a15d660667f02777f564eeeac2f582d7
Robert Schuster <robert.schuster.rs01@gmail.com>
2020-01-16 14:45:46 +0100

    removed test content

--- app/src/test/java/de/tarent/androidws/clean/feature/restaurant/viewmodel/RestaurantListViewModelImplSpec.kt
+++ app/src/test/java/de/tarent/androidws/clean/feature/restaurant/viewmodel/RestaurantListViewModelImplSpec.kt
@@ -23,12 +23,6 @@ import java.lang.IllegalArgumentException
 
 internal class RestaurantListViewModelImplSpec : WordSpec() {
 
-    private lateinit var vm: RestaurantListViewModelImpl
-
-    private val getRestaurantUseCase: GetRestaurantUseCase = mockk()
-
-    private val restaurantItemMapper: RestaurantItemMapper = mockk()
-
     override fun isolationMode(): IsolationMode? = IsolationMode.InstancePerTest
 
     // Provides LiveData updates on JVM main thread
@@ -44,90 +38,10 @@ internal class RestaurantListViewModelImplSpec : WordSpec() {
         mockkStatic(Log::class)
         every { Log.d(any(), any()) } returns 0
 
-        vm = RestaurantListViewModelImpl(
-                getRestaurantUseCase = getRestaurantUseCase,
-                restaurantItemMapper = restaurantItemMapper)
     }
 
     override fun afterTest(testCase: TestCase, result: TestResult) {
         unmockkStatic(Log::class)
     }
 
-    init {
-        "RestaurantListViewModelImpl" should {
-            "be in Initial state after creation" {
-                vm.state.value shouldBe State.Initial
-            }
-        }
-
-        "loadData(false)" should {
-            "set Loading state" {
-                // given:
-                every { getRestaurantUseCase() } returns emptyFlow()
-
-                // when:
-                vm.load(false)
-
-                // then
-                vm.state.value shouldBe State.Loading(isRetryOrInitial = true)
-            }
-
-            "set Content state when data emitted" {
-                // given:
-                every { getRestaurantUseCase() } returns flow {
-                    emit(emptyList())
-                }
-                every { restaurantItemMapper(any()) } returns mockk()
-
-                // when:
-                vm.load(false)
-
-                // then
-                vm.state.value shouldBe State.Content(list = emptyList())
-            }
-
-            "set NetworkError state when IO error flagged" {
-                // given:
-                every { getRestaurantUseCase() } returns flow {
-                    flagIoError("Test IO Error")
-                }
-                every { restaurantItemMapper(any()) } returns mockk()
-
-                // when:
-                vm.load(false)
-
-                // then
-                vm.state.value shouldBe State.NetworkError
-            }
-
-            "set GeneralError state when non-RepositoryException thrown" {
-                // given:
-                every { getRestaurantUseCase() } returns flow {
-                    throw IllegalArgumentException("Just for testing purposes")
-                }
-                every { restaurantItemMapper(any()) } returns mockk()
-
-                // when:
-                vm.load(false)
-
-                // then
-                vm.state.value shouldBe State.GeneralError
-            }
-
-            "set GeneralError state when non-exception thrown" {
-                // given:
-                every { getRestaurantUseCase() } returns flow {
-                    throw Error("Test very grave issue")
-                }
-                every { restaurantItemMapper(any()) } returns mockk()
-
-                // when:
-                vm.load(false)
-
-                // then
-                vm.state.value shouldBe State.GeneralError
-            }
-        }
-
-    }
 }
